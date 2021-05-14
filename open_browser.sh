84ba31dcadfce8663d09dcff2f26844945b82022
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-05-14 16:45:08 +0200

    Delete open_browser.sh

--- open_browser.sh
+++
@@ -1,9 +0,0 @@
-# open_browser.sh
-#! /bin/bash
-#echo $1
-winPath="file://///wsl$//Ubuntu-${20.04}"
-#echo ${winPath}
-#firefox
-/mnt/c/Program\ Files/BraveSoftware/Brave-Browser/Application/brave.exe $winPath
-#chrome
-#/mnt/c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe $winPath
