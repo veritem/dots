cccd262a5105579a3bb98ed491d017a78b6b8194
Verite Mugabo <mugaboverite@gmail.com>
2024-05-13 19:56:58 -0400

    feat(conf): update config for kitty and nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -217,6 +217,9 @@ require('lazy').setup({
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
+  -- for coding starts
+  { 'wakatime/vim-wakatime', lazy = false },
+
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following lua:
   --    require('gitsigns').setup({ ... })
