ec42e733c8e5793eea995bd3defabe835ca0aa3a
Verite Mugabo <mugaboverite@gmail.com>
2024-05-17 01:47:36 -0400

    chore(minor): update

--- nvim/lua/custom/plugins/treesitter.lua
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -19,6 +19,17 @@ return {
     -- @diagnostic disable-next-line: missing-fields
     require('nvim-treesitter.configs').setup(opts)
 
+    -- TODO: Does this reall work?
+    require('nvim-treesitter.parsers').get_parser_configs().just = {
+      install_info = {
+        url = 'https://github.com/IndianBoy42/tree-sitter-just', -- local path or git repo
+        files = { 'src/parser.c', 'src/scanner.c' },
+        branch = 'main',
+        -- use_makefile = true -- this may be necessary on MacOS (try if you see compiler errors)
+      },
+      maintainers = { '@IndianBoy42' },
+    }
+
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
     --
